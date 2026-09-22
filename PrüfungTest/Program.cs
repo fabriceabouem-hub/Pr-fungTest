a2f8450ff627e408a3869fdf589dc8690d04b685
Fabrice Dylan <fabrice.contact@gamail.com>
2026-09-22 13:47:05 +0200

    Projektdateien hinzufügen.

---
+++ PrüfungTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PrüfungTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
